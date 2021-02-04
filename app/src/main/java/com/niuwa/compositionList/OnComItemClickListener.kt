9e22040669a50669d9ebd0652b051965f3271d8d
liupeng <extern.peng.liu1@kuka.com>
2021-02-04 18:15:57 +0800

    关于

--- app/src/main/java/com/niuwa/compositionList/OnComItemClickListener.kt
+++ app/src/main/java/com/niuwa/compositionList/OnComItemClickListener.kt
@@ -3,7 +3,8 @@ package com.niuwa.compositionList
 import android.view.View
 
 interface OnComItemClickListener {
-
+    //列表点击
     fun onClick(view: View?, position: Int,compositionBean: CompositionBean)
+    //列表收藏
     fun collect(status:Boolean)
 }
--- app/src/main/java/com/niuwa/my/AboutUsActivity.kt
+++ app/src/main/java/com/niuwa/my/AboutUsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import com.niuwa.R
+import kotlinx.android.synthetic.main.activity_about_us.*
 
 class AboutUsActivity : WearableActivity() {
 
@@ -12,7 +13,10 @@ class AboutUsActivity : WearableActivity() {
         setContentView(R.layout.activity_about_us)
         // Enables Always-on
         setAmbientEnabled()
-
+        text_member.setOnClickListener {
+            val intent=Intent(this@AboutUsActivity, JoinUsActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
